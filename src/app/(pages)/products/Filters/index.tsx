4fe24d91ae7f922696707d1100618911e345d0b7
Ali Tarichi <tarichiali@gmail.com>
2024-01-16 12:57:45 +0300

    flilters implanted

--- src/app/(pages)/products/Filters/index.tsx
+++ src/app/(pages)/products/Filters/index.tsx
@@ -2,36 +2,67 @@
 
 import React from 'react'
 
-import classes from './index.module.scss'
-import { useFilter } from '../../../_providers/Filter'
 import { Category } from '../../../../payload/payload-types'
 import { Checkbox } from '../../../_components/Checkbox'
 import { HR } from '../../../_components/HR'
+import { RadioButton } from '../../../_components/Radio'
+import { useFilter } from '../../../_providers/Filter'
 
+import classes from './index.module.scss'
 
+const Filters = ({ categories }: { categories: Category[] }) => {
+  const { categoryFilters, sort, setCategoryFilters, setSort } = useFilter()
 
-const Filters = ({categories}: {categories: Category[]}) => {
-  return (
-    const { categoryFilters, sort, setCategoryFilters, setSort } = useFilter()
+  const handleCategories = (categoryId: string) => {
+    if (categoryFilters.includes(categoryId)) {
+      const updatedCategories = categoryFilters.filter(id => id !== categoryId)
 
-    const handleCategories = (categoryId: string) => {
-
+      setCategoryFilters(updatedCategories)
+    } else {
+      setCategoryFilters([...categoryFilters, categoryId])
     }
-    <div className={classes.filter}>
+  }
+
+  const handleSort = (value: string) => setSort(value)
+
+  return (
+    <div className={classes.filters}>
       <div>
         <h6 className={classes.title}>Product Categories</h6>
-        <div className={classes.categories}></div>
-        {categories.map(category => {
-          const isSelected = categoryFilters.includes(category.id)
+        <div className={classes.categories}>
+          {categories.map(category => {
+            const isSelected = categoryFilters.includes(category.id)
 
-          return (
-            <Checkbox key={category.id} label={category.title} value={category.id} isSelected={isSelected} onClickHandler={handleCategories} />
-          )
-        })}
+            return (
+              <Checkbox
+                key={category.id}
+                label={category.title}
+                value={category.id}
+                isSelected={isSelected}
+                onClickHandler={handleCategories}
+              />
+            )
+          })}
+        </div>
+        <HR className={classes.hr} />
+        <h6 className={classes.title}>Sort By</h6>
+        <div className={classes.categories}>
+          <RadioButton
+            label="Latest"
+            value="-createdAt"
+            isSelected={sort === '-createdAt'}
+            onRadioChange={handleSort}
+            groupName="sort"
+          />
+          <RadioButton
+            label="Oldest"
+            value="createdAt"
+            isSelected={sort === 'createdAt'}
+            onRadioChange={handleSort}
+            groupName="sort"
+          />
+        </div>
       </div>
-      <HR className={classes.hr}/>
-      <h6 className={classes.title}>Sort By</h6>
-      <div className={classes.categories}></div>
     </div>
   )
 }
