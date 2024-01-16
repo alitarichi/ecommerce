68a2876648b0ea357fac0302dcbd562f21b798b0
Ali Tarichi <tarichiali@gmail.com>
2024-01-16 12:33:06 +0300

    fixing filters, checkbox broken

--- src/app/(pages)/products/Filters/index.tsx
+++ src/app/(pages)/products/Filters/index.tsx
@@ -1,8 +1,33 @@
+'use client'
+
 import React from 'react'
 
-const Filters = () => {
+import classes from './index.module.scss'
+import { useFilter } from '../../../_providers/Filter'
+import { Category } from '../../../../payload/payload-types'
+import { Checkbox } from 'payload/components/forms'
+
+
+const Filters = ({categories}: {categories: Category[]}) => {
   return (
-    <div>Filters</div>
+    const { categoryFilters, sort, setCategoryFilters, setSort } = useFilter()
+
+    const handleCategories = (categoryId: string) => {
+
+    }
+    <div className={classes.filter}>
+      <div>
+        <h6 className={classes.title}>Product Categories</h6>
+        <div className={classes.categories}></div>
+        {categories.map(category => {
+          const isSelected = false
+
+          return (
+            <Checkbox key={category.id} label={category.title} />
+          )
+        })}
+      </div>
+    </div>
   )
 }
 
